09c6b50eb9c427c3b77cb193a383fe285c41bd01
SAHIL BHARDWAJ <krishnabhardwaj200517@gmail.com>
2026-01-28 12:08:17 +0530

    changed constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:3000/"; //place backend server link
+export const BASE_URL = "/api/"; //place backend server link
 export const DEFAULT_PHOTO_URL =
   "https://i.pinimg.com/736x/7d/93/ab/7d93ab87452ba3d71249c024747a4d56.jpg";
